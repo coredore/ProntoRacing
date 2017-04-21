7634fdbdc69303bfb96d105e9e38348fd60924c2
khemmachattk <khemmachattk@gmail.com>
2017-04-21 15:19:49 +0700

    ligin first

---
+++ js/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyA9HMsOHooM-tMvU5YYqeETIqyfZ4ZMDkI",
+    authDomain: "prontoracing-bc0fc.firebaseapp.com",
+    databaseURL: "https://prontoracing-bc0fc.firebaseio.com",
+    projectId: "prontoracing-bc0fc",
+    storageBucket: "prontoracing-bc0fc.appspot.com",
+    messagingSenderId: "164100522393"
+};
+firebase.initializeApp(config);
